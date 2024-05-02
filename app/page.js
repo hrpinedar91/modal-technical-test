9f4a1e93d5e471a20f41239470bc2f22e18db880
Harold Pineda <movilcenter.depto.tecnologia@gmail.com>
2024-05-02 08:27:38 -0500

    edit stile

--- app/page.js
+++ app/page.js
@@ -4,15 +4,13 @@ import Modal from "@/components/Modal";
 export default function Home() {
   return (
     <>
-    <div className="w-full h-[400px] flex place-content-center">
-      
-      <Modal/>
-    </div>
-    <div className="bg-red-300 w-full h-[400px] mt-8">
-        <h2>Contenido</h2>
+      <div className="w-full h-[400px] flex place-content-center">
+
+        <Modal />
+      </div>
+      <div className="bg-red-300 w-full h-[400px] mt-8">
         <p>Contenido de la página principal</p>
-      </div> 
-    
+      </div>
     </>
   );
 }
